5488b84ee96eada8caf47cddc3d44e8b4a752af8
kevinhsu7361 <applewish9@gmail.com>
2021-03-14 13:04:06 +0800

    加入專案檔案。

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vidly_mvc.Startup))]
+namespace vidly_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
